5e515b880f3bf94968e9cd31176af9d59bb1fe52
chelsofia <rachelasse01@gmail.com>
2025-02-21 00:07:50 +0100

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,9 +14,17 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+
 export const metadata: Metadata = {
-  title: "LanguaFLow",
+  title: "LanguaFlow",
   description: "Text Transformer",
+  other: {
+    "origin-trial": [
+      "AvdIxkVZvIRZkVOyQnBWGpEdJGUhS7OcIqaab5EHWKy4DK9/KX3GCIEOnFYKPZAbeVww5s17PuFNLv26Tzn31gwAAAB8eyJvcmlnaW4iOiJodHRwczovL2xhbmd1YS1mbG93LXVlZmYudmVyY2VsLmFwcDo0NDMiLCJmZWF0dXJlIjoiTGFuZ3VhZ2VEZXRlY3Rpb25BUEkiLCJleHBpcnkiOjE3NDk1OTk5OTksImlzU3ViZG9tYWluIjp0cnVlfQ==",
+      "Ap2UC6TKSYvKiWNmBsm/zcoXLyqeK3PJ/syAT8xaWGdQhywI+9aDr4HgKsd0XUpTS+FUfl4N81C6WCVDDCflCwgAAAB2eyJvcmlnaW4iOiJodHRwczovL2xhbmd1YS1mbG93LXVlZmYudmVyY2VsLmFwcDo0NDMiLCJmZWF0dXJlIjoiVHJhbnNsYXRpb25BUEkiLCJleHBpcnkiOjE3NTMxNDI0MDAsImlzU3ViZG9tYWluIjp0cnVlfQ==",
+      "AusgeH9N3wLbeP0ywntIofAHDja7/C8KyuM/GC54KNNcKVR86+g7Z2LUpAqg8LvZUUJ6RUgnJveZLuEN6N+tmAwAAAB6eyJvcmlnaW4iOiJodHRwczovL2xhbmd1YS1mbG93LXVlZmYudmVyY2VsLmFwcDo0NDMiLCJmZWF0dXJlIjoiQUlTdW1tYXJpemF0aW9uQVBJIiwiZXhwaXJ5IjoxNzUzMTQyNDAwLCJpc1N1YmRvbWFpbiI6dHJ1ZX0=",
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -26,6 +34,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
